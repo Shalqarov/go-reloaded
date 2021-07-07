c5f15aa180b893eea6c34678afa550aa50b79ebc
MangoMango <Temirlan200312@gmail.com>
2021-07-07 19:54:48 +0600

    recursivepower.go

---
+++ recursivepower.go
@@ -0,0 +1,12 @@
+package student
+
+func RecursivePower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	}else if power == 0 {
+		return 1
+	}else if power == 1 {
+		return nb
+	}
+	return nb * RecursivePower(nb, power - 1)
+}
