e824661d3d71a4807a4556eb7683977bb9cafaf5
MangoMango <student@ALEM-f4918d.alem.school>
2021-07-08 16:51:39 +0600

    temp

---
+++ atoi.go
@@ -0,0 +1,35 @@
+package student 
+
+func Atoi(s string) int {
+	if s == "" {
+		return 0
+	}
+	if s[0] == '+' || s[0] == '-' || s[0] >='0' && s[0] <= '9'{
+		startIndex:=0
+		if s[0] == '+' || s[0] == '-' {
+			startIndex = 1
+		}
+		for i:=startIndex; i < len(s); i++{
+			if s[i] < '0' || s[i] > '9' {
+				return 0
+			}
+		}
+		num := 0
+		for i:=startIndex;i<len(s);i++{
+			num = num*10 + int(rune(s[i] - 48))
+		}
+		isNegative:=false
+		if s[0] == '-' {
+			isNegative = true
+			num*=-1
+		}
+		if num > 0 && isNegative == true {
+			return -9223372036854775808
+		} else if num < 0 && isNegative ==false {
+			return 9223372036854775807
+		}
+		return num
+	} else {
+		return 0
+	}
+}
---
+++ test/main.go
@@ -0,0 +1,11 @@
+package main
+
+func PrintCombN(n int) {
+	
+}
+
+func main() {
+	student.PrintCombN(1)
+	student.PrintCombN(3)
+	student.PrintCombN(9)
+}
