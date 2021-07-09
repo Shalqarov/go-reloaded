8d7fa8f12dc3555363461bc902c89b199d7c1b21
MangoMango <student@ALEM-316ecb.alem.school>
2021-07-09 13:49:53 +0600

    09,07

--- test/main.go
+++ test/main.go
@@ -1,5 +1,7 @@
 package main
-import "fmt"
+//import "fmt"
+import "github.com/01-edu/z01"
+
 func Atoi(s string) int {
 	if s == "" {
 		return 0
@@ -36,7 +38,63 @@ func Atoi(s string) int {
 		return 0
 	}
 }
+func RecursivePower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	}else if power == 0 {
+		return 1
+	}else if power == 1 {
+		return nb
+	}
+	return nb * RecursivePower(nb, power - 1)
+}
+func PrintCombN(n int) {
+	num := make([]int,n)
+	for i := 0; i < n; i++ {
+		num[i] = i
+		z01.PrintRune(int32(num[i] + 48))
+	}
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	for {
+		num[n-1] += 1
+		for i:=n - 2; i >= 0; i-- {
+			if num[i+1] >= 10 {
+				num[i] += 1
+			}
+		}
+		for i:=0; i < n -1; i++ {
+			if num[i + 1] >=10 {
+				num[i + 1] = num[i] + 1
+			}
+		}
+		if num[n-1] >= 10 {
+			continue
+		}
+		
+		for i := 0; i < n; i++ {
+			z01.PrintRune(int32(num[i] + 48))
+		}
+		if num[0]>=10-n{
+			z01.PrintRune('\n')
+			break
+		}else {
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		}
+		
+	}
+
+}
+func PrintNbrBase(nbr int, base string) {
+	
+}
+
+// func ActiveBits(n int) uint {
+// }
 
 func main() {
-	fmt.Println(Atoi("13456563454455454535440"))
+	PrintCombN(1)
+	PrintCombN(3)
+	PrintCombN(9)
 }
