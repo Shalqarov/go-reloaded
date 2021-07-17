a4ea0c301463c48daa9e887bdff518b98b156c64
MangoMango <student@ALEM-316ecc.alem.school>
2021-07-18 00:17:04 +0600

    18.07 end

--- rotatevowels/main.go
+++ rotatevowels/main.go
@@ -5,20 +5,50 @@ import (
 	"os"
 
 	"github.com/01-edu/z01"
-	"github.com/01-edu/z01.PrintRune"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	args := os.Args[1:]
+	argsLen := len(args)
+	if argsLen < 1 {
 		z01.PrintRune('\n')
-	}else{
-		args:=os.Args
-		vowels:=[]rune{}
-		for i:=0;i<len(args);i++{
-			vowels = []rune{}
-			for j:=0;j<len(args[i]);j++{
-				if args[i][j] == 
+	} else {
+		str := args[0]
+		for i := 1; i < argsLen; i++ {
+			str += " " + args[i]
+		}
+		fmt.Println(str)
+		vowels := map[rune]bool{'a': true, 'o': true,
+			'u': true, 'e': true, 'i': true,
+			'A': true, 'O': true, 'U': true,
+			'E': true, 'I': true}
+		rotatevowels := []rune{}
+		idx := []int{}
+		for i, symbol := range str {
+			if _, isHave := vowels[symbol]; isHave {
+				rotatevowels = append(rotatevowels, rune(str[i]))
+				idx = append(idx, i)
 			}
+		}
+		rtLen := len(rotatevowels)
+		for i, j := 0, rtLen-1; i < j; i++ {
+			t := rotatevowels[j]
+			rotatevowels[j] = rotatevowels[i]
+			rotatevowels[i] = t
+			j--
+		}
+		res := ""
+		for i, j := 0, 0; i < rtLen; i++ {
+			if i == idx[j] {
+				res += string(rotatevowels[j])
+				j++
+			} else {
+				res += str
+			}
+		}
+		resLen := len(res)
+		for i := 0; i < resLen; i++ {
+			z01.PrintRune(rune(res[i]))
 		}
 	}
 }
