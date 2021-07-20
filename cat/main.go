7d9e8e9e064bc19aff79918dc7965fd0468b6180
MangoMango <student@ALEM-316ecc.alem.school>
2021-07-20 15:47:21 +0600

    cat

--- cat/main.go
+++ cat/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 
 	"github.com/01-edu/z01"
@@ -11,38 +12,21 @@ func main() {
 	args := os.Args[1:]
 	argsSize := len(args)
 	if argsSize == 0 {
-		fmt.Println("entr")
-		//вывод введенной строки
-		data := make([]byte, 0)
-		_, err := os.Stdin.Read(data)
-		if err == nil {
-			datalen := len(data)
-			for i := 0; i < datalen-1; i++ {
-				z01.PrintRune(rune(data[i]))
-			}
-		} else {
-			os.Exit(1)
-		}
-
+		io.Copy(os.Stdout, os.Stdin)
 	} else {
 		for i := 0; i < argsSize; i++ {
-			msg := []rune{}
-			file, err := os.Open(args[i])
+			str, err := ioutil.ReadFile(args[i])
 			if err != nil {
-				msg := []rune{}
-
-				for i := 0; i < len(msg); i++ {
-
-				}
+				printErr("ERROR: " + err.Error() + "\n")
 				os.Exit(1)
 			}
-			f, err := file.Stat()
-			arr := make([]byte, f.Size())
-			file.Read(arr)
-			for i := 0; i < len(arr); i++ {
-				z01.PrintRune(rune(arr[i]))
-			}
-			z01.PrintRune('\n')
+			printErr(string(str))
 		}
+	}
+}
+
+func printErr(s string) {
+	for _, i := range s {
+		z01.PrintRune(i)
 	}
 }
