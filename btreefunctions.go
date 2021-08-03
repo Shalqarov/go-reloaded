26c01d6c6c818cae917794bb826e3e305e2bb653
MangoMango <student@ALEM-316ed1.alem.school>
2021-08-04 00:52:05 +0600

    3 aug

--- btreefunctions.go
+++ btreefunctions.go
@@ -9,14 +9,12 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	if root == nil {
 		return &TreeNode{Data: data}
 	}
-	if root.Data < data {
-		//move right
-		root.Right = BTreeInsertData(root.Right, data)
-		root.Right.Parent = root
-	} else if root.Data > data {
-		//move left
+	if data < root.Data {
 		root.Left = BTreeInsertData(root.Left, data)
 		root.Left.Parent = root
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
 	}
 	return root
 }
